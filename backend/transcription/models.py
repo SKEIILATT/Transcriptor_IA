45d005393943d2bb420b6d41fb396f2bd6dfad87
SKEIILATT <javimacias619@gmail.com>
2025-09-10 23:35:28 -0500

    Configuración final del panel de administración

--- backend/transcription/models.py
+++ backend/transcription/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.utils import timezone
+from zoneinfo import ZoneInfo
 # Create your models here.
 class Transcription(models.Model):
     text = models.TextField(blank=True, null=True, editable=False)
@@ -19,3 +20,8 @@ class Transcription(models.Model):
         default=STATUS_PENDING
     )
     user = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+
+    def __str__(self):
+        zona_local = ZoneInfo("America/Guayaquil")
+        return f"Grabación del día {timezone.localtime(self.uploaded_at, zona_local)}"
+    
